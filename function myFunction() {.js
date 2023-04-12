23a30707bc9bd769dfa9de3ab12274dbb80a7897
chiceeus <130582044+chiceeus@users.noreply.github.com>
2023-04-12 11:16:00 -0400

    Add files via upload

---
+++ function myFunction() {.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById('script').
+    innerHTML="This is a paragraph that has been changed with JavaScript.";
+}
+// Single line comment
+
+/* Testing multi line comment
+testing multi line comment*/
