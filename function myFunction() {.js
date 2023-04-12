abed27b7d7f3663fec461ec23c0c625031bd09d4
chiceeus <130582044+chiceeus@users.noreply.github.com>
2023-04-12 11:26:28 -0400

    Delete function myFunction() {.js

--- function myFunction() {.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    document.getElementById('script').
-    innerHTML="This is a paragraph that has been changed with JavaScript.";
-}
-// Single line comment
-
-/* Testing multi line comment
-testing multi line comment*/
